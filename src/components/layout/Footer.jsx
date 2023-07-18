52a207af35eba869e6ad293dc13619d1d0c09633
Gerardo Moreno <gdmg507@gmail.com>
2023-07-18 15:44:42 -0500

    realizacion de la seccion Sobre mi y algunas modificaciones en el footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -9,7 +9,7 @@ export const Footer = () => {
         <ul className="redes">
             <li><a href="https://www.linkedin.com/in/gerardo-moreno-523626176" target="_blank"><img src="/linkedin.png" alt="linkedin" /></a></li>
             <li><a href="https://github.com/GerardoMorenoDW" target="_blank"><img src="/github.png" alt="github" /></a></li>
-            <li><a href="#" target="_blank"><img src="/portafolio.png" alt="portafolio" /></a></li>
+            <li><a href="https://portafolio-r-gm.netlify.app" target="_blank"><img src="/portafolio.png" alt="portafolio" /></a></li>
             <li><a href="#" target="_blank"><img src="/cv.png" alt="curriculum" /></a></li>
         </ul>
     </footer>
--- src/components/pages/SobreMi.jsx
+++ src/components/pages/SobreMi.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const SobreMi = () => {
   return (
-    <div>SobreMi</div>
+    <div className='jumbo contenedor sobre-mi'>
+      <h1>Gerardo Moreno</h1>
+      <div className='contenedor_imagen'>
+        <img src="/public/fotocarnet.jpg" alt="gerardo moreno foto" />
+      </div>
+      <p className='descripcion'>
+        Mi objetivo profesional es adquirir las habilidades técnicas necesarias para diseñar,
+        desarrollar y mantener aplicaciones web completas y de alta calidad, desde el frontend hasta el back-end. Para lograrlo, me comprometo a aprender y dominar las
+        tecnologías y herramientas fundamentales en ambos lados.
+      </p>
+      <a href="https://portafolio-r-gm.netlify.app" target='_blank'>Portafolio</a>
+    </div>
   )
 }
